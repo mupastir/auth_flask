d40bb121f76a0d8507ef277d12059ab62b09ad3b
mupastir <mupastir@gmail.com>
2019-07-08 12:18:46 +0300

    remove password hash from schema

--- core/schemas/user.py
+++ core/schemas/user.py
@@ -6,6 +6,5 @@ class UserSchema(BaseSchema):
     id = fields.UUID()
     username = fields.Str()
     email = fields.Str()
-    password_hash = fields.Str()
     user_address = fields.Str()
     create_user_date = fields.DateTime()
