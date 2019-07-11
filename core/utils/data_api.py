472ad52d0faced78d5f41ee3cc027d88042d5fef
mupastir <mupastir@gmail.com>
2019-07-11 15:16:49 +0300

    delete test data

--- core/utils/data_api.py
+++ core/utils/data_api.py
@@ -1,7 +1,4 @@
 import requests
-import json
-from uuid import uuid4
-import datetime
 
 
 class AccessToUsers:
@@ -9,11 +6,5 @@ class AccessToUsers:
     @staticmethod
     def get(username):
         response = requests.get(f'http://localhost:5000/user/{username}')
-        # return response
-        return json.dumps({
-            "id": str(uuid4()),
-            "username": username,
-            "email": username + '@gmail.com',
-            "user_address": "Paramonova 25/7",
-            "create_user_date": datetime.datetime.now().__str__()
-        })
+        return response
+
