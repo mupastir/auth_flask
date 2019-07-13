1b57605f02a7b7adc1b9cb8718b790169a1ec0c8
mupastir <mupastir@gmail.com>
2019-07-13 15:42:13 +0300

    delete test data

--- core/utils/data_api.py
+++ core/utils/data_api.py
@@ -13,11 +13,4 @@ class AccessToUsers:
             'password': password
         })
         response = requests.get('http://localhost:5000/user/', params=params)
-        # return response
-        return json.dumps({
-            'user_id': str(uuid4()),
-            'username': username,
-            'email': username + '@urk.net',
-            'user_address': 'address 15/20',
-            'create_user_date': datetime.now().__str__()
-        })
+        return response
